4557bbc78a935e1133661a24b9171283bbf7457d
Luis Fernando <luisfernandomgrs@gmail.com>
2025-01-21 21:10:57 -0300

    Architecture domain: Last step, provide Controller

---
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslist.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+}
