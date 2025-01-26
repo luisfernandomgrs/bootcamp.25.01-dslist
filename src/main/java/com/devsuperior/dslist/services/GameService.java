50a1ea71e72202bce8bb08f637e1b22f7489660e
Luis Fernando <luisfernandomgrs@gmail.com>
2025-01-26 19:24:17 -0300

    Projections, GET group of games by list id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -2,6 +2,7 @@ package com.devsuperior.dslist.services;
 
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,5 +24,9 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<Game> findAll() {
         return gameRepository.findAll();
+    }
+    @Transactional(readOnly = true)
+    public List<GameMinProjection> findByList(Long listId) {
+        return gameRepository.searchByList(listId);
     }
 }
